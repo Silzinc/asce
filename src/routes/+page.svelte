add9cf25109da0c1ea8fb1612eaa01ebd1251bde
Silzinc <jonasbloch85@gmail.com>
2024-04-06 15:03:00 +0200

    Welcoming page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import Elec from 'virtual:icons/material-symbols/electric-bolt-rounded';
+</script>
+
+<div class="container h-full mx-auto flex justify-center items-center">
+	<div class="space-y-10 text-center flex flex-col items-center">
+		<h2 class="h2">Welcome to Asce.</h2>
+		<!-- Animated Logo -->
+		<figure>
+			<section class="img-bg" />
+			<Elec class="w-64 h-64 md:w-80 md:h-80 fill-token" />
+		</figure>
+		<!-- / -->
+		<div class="flex justify-center space-x-2">
+			<a class="btn variant-filled" href="https://skeleton.dev/" target="_blank" rel="noreferrer">
+				Launch Skeleton-UI Documentation
+			</a>
+		</div>
+		<div class="space-y-2"></div>
+	</div>
+</div>
